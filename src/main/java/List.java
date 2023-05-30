0a607d234e51af898fd54eadc229e3a73d676486
ViktoriaKukh <bstet@mail.ru>
2023-05-31 01:15:42 +0500

    reversList

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,4 +1,33 @@
-public class HeapSort {
+public class List {
+    Node head;
+    Node tail;
+    public Node find(int value) {
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.value == value) {return currentNode};
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
 
+    public void reversList(){
+        Node currentNode = head;
+        head = tail;
+        tail = currentNode;
+        while (currentNode != null){
+            Node temp = currentNode.next;
+            currentNode.next = currentNode.previous;
+            currentNode.previous = temp;
+            currentNode = temp;
+        }
+    }
+
+
+
+    public class Node{
+    int value;
+    Node next;
+    Node previous;
+    }
 
 }
